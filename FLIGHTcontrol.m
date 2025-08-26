f6ca15b513eaf54e15ea82d43f0ef72ff68a826c
matthew mangano <matthew.mangano@gmail.com>
2025-08-25 20:33:07 -0700

    removed flight control

--- FLIGHTcontrol.m
+++
@@ -1,9 +0,0 @@
-function figH = FLIGHTcontrol ()
-%FLIGHT This is a wrapper for starting the t
-    persistent obj
-    Application.removeInstallsFromMPath();
-    addpath(fileparts(mfilename('fullpath')));
-    obj = CTRLMain; 
-    figH = obj.Figure;
-end
-
